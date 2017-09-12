5984285f8ec44b457b12330d85c6c83b9cf76f76
Hanna Hakhova <h.hakhova@levi9.com>
2017-09-12 12:09:54 +0300

    Commit #5

--- src/scripts/components/Todo/index.test.js
+++ src/scripts/components/Todo/index.test.js
@@ -2,10 +2,24 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import Todo from './index';
 
-const container = shallow(<Todo toggle={() => {}} todo={{}}/>);
-
 describe('Todo component', () => {
-    it('should match expected snapshot', () => {
-        expect(container).toMatchSnapshot();
+    it('with empty todo object should match expected snapshot', () => {
+        expect(shallow(<Todo toggle={() => {}} todo={{id: 1, text: 'Do task1', isDone: false}}/>)).toMatchSnapshot();
+    });
+
+    it('with not empty todo object should match expected snapshot', () => {
+        expect(shallow(<Todo toggle={() => {}} todo={{id: 1, text: 'Do task1', isDone: true}}/>)).toMatchSnapshot();
     });
 });
+
+// jest.doMock('./Todo', () => {
+//   const ComponentToMock = () => <div />;
+//   return ComponentToMock;
+// });
+
+// const Todo = require('./Todo').default;
+// // it('should display text of todo if todo.isDone is true', () => {
+// //     const todo = {id: 1, text: 'Some text', isDone: true};
+// //     const expectedResult = 'Some text';
+// //     expect(expectedResult).toEqual(todo.text);
+// //     });
--- src/scripts/containers/TodoApp/index.test.js
+++ src/scripts/containers/TodoApp/index.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-import TodoPage from './index';
+import {func} from 'prop-types';
+import {shallow, spy, mount} from 'enzyme';
+import TodoApp from './index';
 
-describe('TodoPage', () => {
-    it('TodoPage match expected snapshot', () => {
-        expect(shallow(<TodoPage/>)).toMatchSnapshot();
+const mockTodoList = jest.mock('components/TodoList', () => {
+    const todo = {
+        id: 1,
+        text: 'Do task1',
+        isDone: false
+    };
+    const TodoList = ({toggle}) => <div id={'mockId'} onClick={() => { toggle(todo); }}/>;
+    TodoList.propTypes = {
+        toggle: func
+    };
+    return TodoList;
+});
+
+describe('TodoApp', () => {
+    it('TodoApp match expected snapshot', () => {
+        expect(shallow(<TodoApp/>)).toMatchSnapshot();
     });
 });
+
+it('calls toggle the button click', () => {
+    mockTodoList.find('button').simulate('click');
+    expect(mockTodoList.state(mockTodoList.id).value).toEqual({
+        id: 1,
+        text: 'Do task1',
+        isDone: true
+    });
+});
+
